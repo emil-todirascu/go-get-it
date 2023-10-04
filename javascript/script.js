72a86758bd0543cdc8019a11acabde9dd0f33a7e
Emil Todirascu <emil.todirascu@gmail.com>
2023-10-04 17:47:18 +0200

    added alert about features

--- javascript/script.js
+++ javascript/script.js
@@ -338,3 +338,29 @@ for (let i = 0; i < windows.length; i++) {
 	// console.log(i);
 	dragElement(windows[i]);
 }
+
+alert(`
+Hello,
+This project is not finished yet; however I have left here 
+a small demo (if you can even call it a demo) which has 
+all the features that have been implemented as of now.
+
+Features:
+  - Tabs:
+	- moveable/resizable
+	- opening/closing
+	- minimize/maximize functions
+	- snapping to cover a half/quarter the screen
+  - Applications:
+	- Command Based Console (CBC)
+	- Notepad
+  - CBC:
+	- command line interface with functions to
+	   navigate and make/delete files and directories
+	- network and some other command not implemented yet
+	- type "help" to get started
+  - Notepad:
+	- basic notepad where you can write notes
+	- saves text when minimized
+
+`);
